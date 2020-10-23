9edb9f27fa0122fd1f65fec44147faa5b6be91cf
R-Prince <reneprince93@icloud.com>
2020-10-23 16:16:55 +0000

    Style index.html using CSS and bootstrap

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 from datetime import datetime
 
 app = Flask(__name__)
@@ -10,13 +10,7 @@ app.secret_key = "randomstring123"
 def add_message(username, message):
     # Add message to the messages list with time stamp
     now = datetime.now().strftime("%H:%M:%S")
-    messages_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(messages_dict)
-
-
-def get_all_messages():
-    # Adding a break between all messages
-    return "<br>".join(messages)
+    messages.append({"timestamp": now, "from": username, "message": message})
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -25,30 +19,23 @@ def index():
     if request.method == "POST":
         session["username"] = request.form["username"]
     if "username" in session:
-        return redirect(session["username"])
+        return redirect(url_for('user', username=session['username']))
 
     return render_template("index.html")
 
 
 @app.route("/<username>", methods=["GET", "POST"])
 def user(username):
-    # Display chat message
+    # Display and add chat messages
 
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
         add_message(username, message)
-        return redirect(session["username"])
+        return redirect(url_for('user', username=session['username']))
 
     return render_template(
         'chat.html', username=username, chat_messages=messages)
-
-
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    # Create chat message and redirect back to chat page
-    add_message(username, message)
-    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
