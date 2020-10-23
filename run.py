50417516b39ab1818400164d1bb696ccc8e542f2
R-Prince <reneprince93@icloud.com>
2020-10-23 10:07:02 +0000

    Add basic routing and view for username and message

--- run.py
+++ run.py
@@ -3,9 +3,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    # Main page instructions
+    return "To send a message use /Username/Message "
 
 
-app.run(host=os.getenv("IP"),port=int(os.getenv("PORT")),debug=True)
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username.title()
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return f"{username.title()}: {message}"
+
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
