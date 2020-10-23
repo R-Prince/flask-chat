1f59b5bd8d2931fe80d3de8ee681ee8c48718f48
R-Prince <reneprince93@icloud.com>
2020-10-23 10:41:10 +0000

    Display all messages in a list format

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+
+def add_message(username, message):
+    # Add message to the messages list
+    messages.append(f"{username}: {message}")
+
+
+def get_all_messages():
+    # Adding a break between all messages
+    return "<br>".join(messages)
 
 
 @app.route("/")
@@ -12,12 +23,15 @@ def index():
 
 @app.route("/<username>")
 def user(username):
-    return "Hi " + username.title()
+    # Display chat message
+    return f"<h1>Welcome {username.title()}</h1> {get_all_messages()}"
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    return f"{username.title()}: {message}"
+    # Create chat message and redirect back to chat page
+    add_message(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
