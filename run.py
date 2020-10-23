580fee28d06e0b50a8718a5b66049cd9599f49db
R-Prince <reneprince93@icloud.com>
2020-10-23 19:06:50 +0000

    Fix username error on homepage

--- run.py
+++ run.py
@@ -1,43 +1,43 @@
 import os
-from flask import Flask, redirect, render_template, request, session
 from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
-messages = []
 app.secret_key = os.getenv("SECRET", "randomstring123")
+messages = []
 
 
 def add_message(username, message):
-    # Add message to the messages list with time stamp
+    """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(messages_dict)
+    messages.append({"timestamp": now, "from": username, "message": message})
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # Prompt user to supply username and then direct to personal homepage
+    """Main page with instructions"""
     if request.method == "POST":
         session["username"] = request.form["username"]
+
     if "username" in session:
-        redirect(session["username"])
+        return redirect(url_for("user", username=session["username"]))
 
     return render_template("index.html")
 
 
-@app.route("/<username>", methods=["GET", "POST"])
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    # Display and add chat messages
-
+    """Add and display chat messages"""
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
         add_message(username, message)
-        return redirect(session["username"])
+        return redirect(url_for("user", username=session["username"]))
 
-    return render_template(
-        "chat.html", username=username, chat_messages=messages)
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
-app.run(host=os.getenv(
-    "IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=True)
